df345066e139fea288ec5bd21d52236cfa196ac7
McKenzie Fiege <mfiege@jwplayer.com>
2019-06-19 14:46:15 -0400

    inital setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `McKenzie Fiege`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,11 +18,11 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        start_url: `/`
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
+        // display: `minimal-ui`,
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
