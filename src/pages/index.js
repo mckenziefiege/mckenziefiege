9eb80c190d793eeb81afb87241725eb09a589e0b
McKenzie Fiege <mfiege@jwplayer.com>
2019-06-27 17:42:33 -0400

    added name to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,8 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <div>
+        <h1>McKenzie Fiege</h1>
+        <h2>Full Stack Developer</h2>
         {renderProjects()}
       </div>
     )
